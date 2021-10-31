cd505023db24ce8aef4bc8e1a218f6e142944f49
user.email <tramtimmy89@gmail.com>
2021-10-31 13:59:34 -0700

    in registration form, added little eye icon to toggle password visibility on and off again.

--- application/public/js/formValidation.js
+++ application/public/js/formValidation.js
@@ -29,6 +29,29 @@ confirmPasswordElement.addEventListener('input', (event) => { valid_confirm_pass
 confirmPasswordElement.addEventListener('focus', () => {showDisplay(confirmPasswordErrorContainer)});
 confirmPasswordElement.addEventListener('blur', () => {hideDisplay(confirmPasswordErrorContainer)});
 
+let eyecon = document.querySelectorAll('.eye-con');
+eyecon.forEach((item) => {
+    var itemClass = item;
+    item.addEventListener('click', () => {
+        togglePassword(itemClass, itemClass.parentNode);
+    });
+});
+
+
+/**
+ * Toggles password input from password to text and font awesome eye to slash and non-slash eye.
+ * @param {*} eye a span tag that contains a child <i class="fas {some font awesome icon here}">.
+ * @param {*} container a div that is the parent of eye.
+ */
+function togglePassword(eye, container) { 
+    const passwordInput = container.children[0];
+    const fontAwesomeEye = eye.children[0];
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    const eyeType =  fontAwesomeEye.getAttribute('class') === 'fas fa-eye' ? 'fas fa-eye-slash' : 'fas fa-eye';
+    passwordInput.setAttribute('type', type);
+    fontAwesomeEye.className = eyeType;
+}
+
 /**
  * validates form by checking global booleans
  * @returns true or false depending if all criteria has been met.
