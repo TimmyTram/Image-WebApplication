405926a0281fba46c9481dd8dc92b19d00583189
Timmy Tram <tramtimmy89@gmail.com>
2021-11-27 18:10:31 -0800

    implemented comment route

--- application/routes/comments.js
+++ application/routes/comments.js
@@ -4,7 +4,39 @@ const { successPrint, errorPrint } = require('../helpers/debug/debugprinters');
 const { create } = require('../models/Comments');
 
 router.post('/create', (req, res, next) => {
-    console.log(req.body);
+    if(!req.session.username) {
+        errorPrint("Must be logged in to comment.");
+        res.json({
+            code: -1,
+            status: "danger",
+            message: "Must be logged in to create a comment"
+        });
+    } else {
+        let {comment, postId} = req.body;
+        let username = req.session.username;
+        let userId = req.session.userId;
+        create(userId, postId, comment)
+        .then((wasSuccessful) => {
+            if(wasSuccessful !== -1) {
+                successPrint(`Comment was created for ${username}`);
+                res.json({
+                    code: 1,
+                    status: "success",
+                    message: "Comment created.",
+                    comment : comment,
+                    username : username
+                });
+            } else {
+                errorPrint("Comment was not saved.");
+                res.json({
+                    code: -1,
+                    status: "danger",
+                    message: "Comment was not created."
+                });
+            }
+        })
+        .catch((err) => next(err));
+    }
 });
 
 
